0c71907ac6d96183b61baeae483793808c58689e
unknown <Danirl@.(none)>
2011-02-23 17:03:59 -0500

    Initial commit of drupal 7 core.

---
+++ modules/user/user-picture.tpl.php
@@ -0,0 +1,22 @@
+<?php
+// $Id: user-picture.tpl.php,v 1.6 2011/01/24 12:49:34 dries Exp $
+
+/**
+ * @file
+ * Default theme implementation to present a picture configured for the
+ * user's account.
+ *
+ * Available variables:
+ * - $user_picture: Image set by the user or the site's default. Will be linked
+ *   depending on the viewer's permission to view the user's profile page.
+ * - $account: Array of account information. Potentially unsafe. Be sure to
+ *   check_plain() before use.
+ *
+ * @see template_preprocess_user_picture()
+ */
+?>
+<?php if ($user_picture): ?>
+  <div class="user-picture">
+    <?php print $user_picture; ?>
+  </div>
+<?php endif; ?>
